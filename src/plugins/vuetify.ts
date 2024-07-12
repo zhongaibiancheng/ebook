fd2a3b87f52576af67279e409d282de1d1333b1e
Joel Young [Jooser PC] <joeleyoung01@gmail.com>
2024-07-11 22:23:06 -0500

    Removed useless comment

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,6 +1,6 @@
 // Vuetify
 import "vuetify/styles";
-import "@mdi/font/css/materialdesignicons.css"; // Ensure you are using css-loader
+import "@mdi/font/css/materialdesignicons.css";
 import { createVuetify, type ThemeDefinition } from "vuetify";
 
 // To customize SASS variables, follow the guide here: https://vuetifyjs.com/en/features/sass-variables
